943ede7fadf37627ffe5122539290b755829ea45
afsana-jannat <afsanajannat205@gmail.com>
2023-12-24 00:27:46 +0600

    all okay

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.port || 5000;
@@ -25,15 +25,53 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
+        const taskCollection = client.db("taskUser").collection("tasks")
+        const userCollection = client.db("taskUser").collection("users")
+        //  tasks collection
+        app.get('/tasks', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await taskCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/uniqueOccupations', async (req, res) => {
+            const uniqueOccupations = await userCollection.distinct('occupation');
+            res.send({ uniqueOccupations });
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        app.post('/tasks', async (req, res) => {
+            const taskItem = req.body;
+            const result = await taskCollection.insertOne(taskItem);
+            res.send(result);
+        })
+
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.deleteOne(query);
+            res.send(result);
+        })
 
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
